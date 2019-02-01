f3ef5b5ac68e1b23905a7c7d124c10d0bd8eb8e8
joshu <joshu@192.168.1.6>
2019-02-01 18:47:08 -0500

    Calculatorlib Project

---
+++ calculator-lib/src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+
+/**
+ * Calculator Class
+ * @author J.Francis
+ * 
+ **/
+public class Calculator {
+	/**
+	 * Multiplies <i><strong>three</strong></i> numbers.
+	 * @param num 1 First number
+	 * @param num 2 Second number
+	 * @param num 3 Third number
+	 * @return Product of three numbers
+	 **/
+	public float multiply(float num1, float num2, float num3) {
+		return num1*num2*num3;
+	}
+}
